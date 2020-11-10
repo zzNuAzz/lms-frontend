0d09d620c81ec9af56e5c37c39d003e5df763e7f
fuzeless <lequangnhat2000@gmail.com>
2020-11-10 17:28:01 +0700

    Some CourseCard component remake

---
+++ src/Components/course-card/course-card.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  //* Basic properties
+  border-radius: 4px;
+  background: #fff;
+  // box-shadow: 0 6px 10px rgba(0, 0, 0, .08), 0 0 6px rgba(0, 0, 0, .05);
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: .3s transform cubic-bezier(.155, 1.105, .295, 1.12), .3s box-shadow, .3s -webkit-transform cubic-bezier(.155, 1.105, .295, 1.12);
+  // padding: 14px 80px 18px 36px;
+  padding: 30px 30px;
+  padding-right: 80px;
+  cursor: pointer;
+
+  //* Background properties
+  background-image: url('https://ionicframework.com/img/getting-started/components-card.png');
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: 80px;
+
+  //* On hover action
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
+`;
+
+// eslint-disable-next-line import/prefer-default-export
+export const CourseCard = ({ title, id }) => (
+  <>
+    <Card>
+      <h3>{title}</h3>
+      <p>{id}</p>
+    </Card>
+  </>
+);
