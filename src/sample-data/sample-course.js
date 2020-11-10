014a86401ec05bd1a48316125629156439a21c2d
fuzeless <lequangnhat2000@gmail.com>
2020-11-10 16:29:42 +0700

    Add SampleCourse for testing

---
+++ src/sample-data/sample-course.js
@@ -0,0 +1,34 @@
+const Courses = [
+  {
+    name: 'Phát triển Web',
+    id: 'INT3306_21',
+    host: 'Lê Quang Nhật',
+  },
+  {
+    name: 'Lý thuyết thông tin',
+    id: 'INT2091_21',
+    host: 'Lê Khôi',
+  },
+  {
+    name: 'Xác suất thống kê',
+    id: 'MAT2019_21',
+    host: 'Đặng Cao Cường',
+  },
+  {
+    name: 'Phân tích thiết kế hướng đối tượng',
+    id: 'INT2028_21',
+    host: 'Hạnh',
+  },
+  {
+    name: 'Quản lý dự án',
+    id: 'INT2021_21',
+    host: 'Phạm Ngọc Hùng',
+  },
+  {
+    name: 'Kiểm thử',
+    id: 'INT1022_21',
+    host: '?????????',
+  },
+];
+
+export default Courses;
