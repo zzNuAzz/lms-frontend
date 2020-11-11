5beb0b2b6a55031f33e7675543579525c01f437c
fuzeless <lequangnhat2000@gmail.com>
2020-11-11 22:33:00 +0700

    Temporary use fetch() for querying instead of axios

--- src/api/graphql-fetch.js
+++ src/api/graphql-fetch.js
@@ -1,9 +1,15 @@
-import { sendPostRequest } from './api-send';
+// import { sendPostRequest } from './api-send';
 
 // eslint-disable-next-line import/prefer-default-export
-export const graphQLFetch = async ({ query, variables = {} }) => {
-  const url = '/graphql';
-  const result = await sendPostRequest(url, JSON.stringify({ query, variables }));
+export const graphQLFetch = async (query, variables = {}) => {
+  // const result = await sendPostRequest(url, JSON.stringify({ query, variables }));
+  const result = await fetch('/api/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query, variables }),
+  });
   if (result) {
     return result.data;
   }
