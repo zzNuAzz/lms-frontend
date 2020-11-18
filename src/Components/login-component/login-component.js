a6f46a2ba86bec66a2c9ba2060ac85d27ac194e5
fuzeless <lequangnhat2000@gmail.com>
2020-11-18 19:02:23 +0700

    Separate LoginPage to LoginComponent (for reuse)

---
+++ src/Components/login-component/login-component.js
@@ -0,0 +1,136 @@
+/* eslint-disable no-undef */
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import userLogin from '../../api/user-login';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    // marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  h5: {
+    margin: 20,
+  },
+}));
+
+function LoginComponent({ setUsername, setLoginStatus, setModalOpen }) {
+  const classes = useStyles();
+  const [formUsername, setFormUsername] = useState('');
+  const [formPassword, setFormPassword] = useState('');
+
+  const handleUserSignIn = async () => {
+    const result = await userLogin(formUsername, formPassword);
+
+    if (result.code === 200) {
+      toast.success(`😃 Welcome ${result.credentials.user.username}`);
+      setUsername(formUsername);
+      sessionStorage.setItem('username', formUsername);
+      sessionStorage.setItem('userId', result.credentials.user.userId);
+    } else if (result.code === 400 || 401) {
+      toast.error('😞 Wrong username or password, please try again!');
+    }
+  };
+
+  return (
+    <>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar
+            className="classes.avatar"
+            alt="Remy Sharp"
+            src="https://w7.pngwing.com/pngs/797/100/png-transparent-course-training-class-professional-certification-education-courses-miscellaneous-angle-business.png"
+          />
+          <Typography component="h1" variant="h5" className={classes.h5}>
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              onChange={(event) => setFormUsername(event.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(event) => setFormPassword(event.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleUserSignIn}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link to="/signup" variant="body2">
+                  Don't have an account? Sign Up
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default LoginComponent;
