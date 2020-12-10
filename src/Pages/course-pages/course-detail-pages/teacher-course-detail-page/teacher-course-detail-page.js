6aaea130b2200656cbd17ff98b44ef75e920dcb7
fuzeless <lequangnhat2000@gmail.com>
2020-12-10 18:35:09 +0700

    Vertical tab testing

--- src/Pages/course-pages/course-detail-pages/teacher-course-detail-page/teacher-course-detail-page.js
+++ src/Pages/course-pages/course-detail-pages/teacher-course-detail-page/teacher-course-detail-page.js
@@ -1,4 +1,10 @@
-import { LinearProgress, Tab, Tabs } from '@material-ui/core';
+import {
+  LinearProgress,
+  Tab,
+  Tabs,
+  makeStyles,
+  Grid,
+} from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import SubjectRoundedIcon from '@material-ui/icons/SubjectRounded';
@@ -9,7 +15,6 @@ import PersonPinRoundedIcon from '@material-ui/icons/PersonPinRounded';
 import PeopleRoundedIcon from '@material-ui/icons/PeopleRounded';
 import { toast } from 'react-toastify';
 
-import { getCourseById } from '../../../../api/graphql/get-course-by-id';
 import CourseCardLarge from '../../../../Components/common-components/course-card/course-card-large';
 import OverviewComponent from '../../../../Components/common-components/course-detail-components/overview-component/overview-component';
 import AssignmentsComponent from '../../../../Components/common-components/course-detail-components/assignments-component/assignments-component';
@@ -24,6 +29,8 @@ import getCourseDetails from '../../../../api/graphql/get-course-details';
 import toastFetchErrors from '../../../../Components/tools/toast-fetch-errors';
 
 const TeacherCourseDetailPage = () => {
+  // const classes = useStyles();
+
   const { id } = useParams();
   const [isLoading, setLoading] = useState(false);
   const [courseName, setCourseName] = useState('');
@@ -149,8 +156,8 @@ const TeacherCourseDetailPage = () => {
   }, []);
 
   const RenderComponent = (
-    <>
-      <CourseCardLarge
+    <div>
+      {/* <CourseCardLarge
         courseName={courseName}
         courseId={courseId}
         courseLecturer={
@@ -159,98 +166,110 @@ const TeacherCourseDetailPage = () => {
             : 'Lecturer has...no name?'
         }
       />
-      <br />
-      <Tabs
-        variant="scrollable"
-        scrollButtons="on"
-        value={tabPosition}
-        onChange={(event, newPos) => setTabPosition(newPos)}
-        indicatorColor="primary"
-        textColor="primary"
+      <br /> */}
+      <Grid
+        container
+        direction="row"
+        justify="center"
       >
-        <Tab
-          label={(
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <SubjectRoundedIcon />
+        <Grid item md="2">
+          <Tabs
+            orientation="vertical"
+            variant="standard"
+            scrollButtons="on"
+            value={tabPosition}
+            onChange={(event, newPos) => setTabPosition(newPos)}
+            indicatorColor="primary"
+            textColor="primary"
+          >
+            <Tab
+              label={(
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  <SubjectRoundedIcon />
               &nbsp;
               Overview
-            </div>
-          )}
-        />
-        <Tab
-          label={(
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <PeopleRoundedIcon />
+                </div>
+              )}
+            />
+            <Tab
+              label={(
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  <PeopleRoundedIcon />
               &nbsp;
               Members
-            </div>
-          )}
-        />
-        <Tab
-          label={(
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <DescriptionRoundedIcon />
+                </div>
+              )}
+            />
+            <Tab
+              label={(
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  <DescriptionRoundedIcon />
               &nbsp;
               Documents
-            </div>
-          )}
-        />
-        <Tab
-          label={(
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <BorderColorRoundedIcon />
+                </div>
+              )}
+            />
+            <Tab
+              label={(
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  <BorderColorRoundedIcon />
               &nbsp;
               Assignments
-            </div>
-          )}
-        />
-        <Tab
-          label={(
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <ForumRoundedIcon />
+                </div>
+              )}
+            />
+            <Tab
+              label={(
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  <ForumRoundedIcon />
               &nbsp;
               Forum
-            </div>
-          )}
-        />
-        <Tab
-          label={(
-            <div style={{ display: 'flex', flexDirection: 'row' }}>
-              <PersonPinRoundedIcon />
+                </div>
+              )}
+            />
+            <Tab
+              label={(
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  <PersonPinRoundedIcon />
               &nbsp;
               Contact
-            </div>
-          )}
-        />
-      </Tabs>
+                </div>
+              )}
+            />
+          </Tabs>
+        </Grid>
+        <Grid item md="10">
+          {(() => {
+            switch (tabPosition) {
+              case 0:
+                return <OverviewComponent currentDescription={courseDescription} fetchCourseDetails={fetchCourseDetails} />;
+              case 1:
+                return (
+                  <CourseMembersComponent
+                    courseId={id}
+                    enrolledMembers={enrolledMembers}
+                    pendingMembers={pendingMembers}
+                    rejectedMembers={rejectedMembers}
+                    fetchAllMembers={fetchAllMembers}
+                  />
+                );
+              case 2:
+                return <DocumentComponent />;
+              case 3:
+                return <AssignmentsComponent assignments={assignments} courseId={id} fetchAssignments={fetchAssignments} />;
+              case 4:
+                return <ForumComponent courseId={courseId} />;
+              case 5:
+                return <ContactComponent user={host} />;
+              default:
+                return null;
+            }
+          })()}
+        </Grid>
+      </Grid>
       <br />
-      {(() => {
-        switch (tabPosition) {
-          case 0:
-            return <OverviewComponent currentDescription={courseDescription} fetchCourseDetails={fetchCourseDetails} />;
-          case 1:
-            return (
-              <CourseMembersComponent
-                courseId={id}
-                enrolledMembers={enrolledMembers}
-                pendingMembers={pendingMembers}
-                rejectedMembers={rejectedMembers}
-                fetchAllMembers={fetchAllMembers}
-              />
-            );
-          case 2:
-            return <DocumentComponent />;
-          case 3:
-            return <AssignmentsComponent assignments={assignments} courseId={id} fetchAssignments={fetchAssignments} />;
-          case 4:
-            return <ForumComponent courseId={courseId} />;
-          case 5:
-            return <ContactComponent user={host} />;
-          default:
-            return null;
-        }
-      })()}
-    </>
+
+    </div>
   );
 
   return (
