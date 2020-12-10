7a180df038b85eeb1a28c278a49f456d042099bd
fuzeless <lequangnhat2000@gmail.com>
2020-12-10 19:04:01 +0700

    Code reformatting?

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -7,69 +7,62 @@ import Footer from '../Components/Footer/Footer';
 import routes from './routes.config';
 
 function Page() {
-    return (
-        <>
-            <Switch>
-                <Redirect exact from="/" to="/home" />
-                {routes.map(route)}
-            </Switch>
-        </>
-    );
+  return (
+    <>
+      <Switch>
+        <Redirect exact from="/" to="/home" />
+        {routes.map(route)}
+      </Switch>
+    </>
+  );
 }
 
-const route = route => {
-	const { hasContainer, hasNavbar, hasFooter, component, path } = route;
-	let content = component;
-    content = hasContainer ?  withContainer(content) : content;
-    content = hasNavbar ? withNavbar(content) : content;
-    content = hasFooter ?  withFooter(content) : content;
-    return <Route key={path} component={content} path={path} />;
+const route = (route) => {
+  const {
+    hasContainer, hasNavbar, hasFooter, component, path,
+  } = route;
+  let content = component;
+  content = hasContainer ? withContainer(content) : content;
+  content = hasNavbar ? withNavbar(content) : content;
+  content = hasFooter ? withFooter(content) : content;
+  return <Route key={path} component={content} path={path} />;
 };
 
-const withContainer = component => {
-	return	() => (
-		<Container maxWidth="lg">{component()}</Container>
-	);
-}
+const withContainer = (component) => () => (
+  <Container maxWidth="lg">{component()}</Container>
+);
 
-const withToastContainer = Component => {
-    return () => (
-        <Fragment>
-            <ToastContainer
-                position="bottom-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-            <Component />
-        </Fragment>
-    );
-};
+const withToastContainer = (Component) => () => (
+  <>
+    <ToastContainer
+      position="bottom-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+    <Component />
+  </>
+);
 
-const withNavbar = Component => {
-	return () => (
-		<Fragment>
-			<NavBar />
-			<Component />
-		</Fragment>
-	);
-}
+const withNavbar = (Component) => () => (
+  <>
+    <NavBar />
+    <Component />
+  </>
+);
 
-const withFooter = Component => {
-	return () => (
-		<Fragment>
-			<div style={{minHeight: '90vh'}}>
-				<Component/>
-			</div>
-			<Footer />
-		</Fragment>
-	);
-}
-
+const withFooter = (Component) => () => (
+  <>
+    <div style={{ minHeight: '90vh' }}>
+      <Component />
+    </div>
+    <Footer />
+  </>
+);
 
 export default withToastContainer(Page);
